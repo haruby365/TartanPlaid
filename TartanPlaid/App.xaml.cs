47f24dce150ab98c4310de58f64d2cd1563fcaa3
username <email@example.com>
2021-09-28 12:55:46 +0900

    초기 커밋.

---
+++ TartanPlaid/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿// © 2021 Jong-il Hong
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Haruby.TartanPlaid
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
